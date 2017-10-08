0d096d9cbd3d826a6a7f873c26272a64fc84d3be
Sergio <Sergio@Mac-mini-de-Sergio.local>
2017-10-08 20:02:01 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/docs', function (req, res) {
+  res.sendfile('docs.html');
+});
+
+app.get('/topic', function (req, res) {
+  res.sendfile('topic.html');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
